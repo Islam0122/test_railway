9448b0a28385fae070e82c721bea7bb617221fcb
islam <dujsobaevislam01@gmail.com>
2024-11-13 00:56:38 +0600

    fgad;bkhjkl;'7-----------yyy

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .drf_yasg import urlpatterns as urls_swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app.main.urls')),
 
-]
+]+ urls_swagger
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
